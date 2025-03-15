1148bc19086430a30f31203453b56ebf97fee4ca
ma-muktadeer <muktadeer.ma@gmail.com>
2025-03-15 14:52:00 +0600

    ok

--- src/test/hacker/math/sort/Invariant_Loop/Result.java
+++ src/test/hacker/math/sort/Invariant_Loop/Result.java
@@ -14,35 +14,28 @@ public class Result {
         }
         for (int i = 0; i < n - 1; i++) {
             for (int j = 1; j < n; j++) {
-                s = s + isGreater(res, res[j-1], res[j], j);
-                
+                s = s + isGreater(res, res[j - 1], res[j], j);
+
             }
             Stream.of(res).forEach(f -> System.out.print(f + " "));
             System.out.println(s);
         }
 
         Stream.of(res).forEach(f -> System.out.print(f + " "));
-       System.out.print(s);
+        System.out.print(s);
     }
 
-    private static Integer isGreater(Integer[] arr,Integer mValue, Integer checkValue, int pos) {
+    private static Integer isGreater(Integer[] arr, Integer mValue, Integer checkValue, int pos) {
         int s = 0;
-        int i = 0;
-        // for (int i = 0; i < pos; i++) {
-            // while (i<pos) {
-            if (mValue > checkValue) {
-                s++;
-                int rp = arr[pos-1];
-                arr[pos-1] = checkValue;
-                arr[pos] = rp;
-                checkValue = rp;
-            // }else{
-            //     return s;
-            // }
-            i++;
+        if (mValue > checkValue) {
+            s++;
+            int rp = arr[pos - 1];
+            arr[pos - 1] = checkValue;
+            arr[pos] = rp;
+            checkValue = rp;
         }
-        System.out.println("sort= "+s);
+        System.out.println("sort= " + s);
         return s;
     }
-    
+
 }
--- src/test/hacker/math/sort/SortAdvanced/Result.java
+++ src/test/hacker/math/sort/SortAdvanced/Result.java
@@ -1,31 +1,33 @@
 package test.hacker.math.sort.SortAdvanced;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Result {
-    public static int insertionSort(List<Integer> arr) {
+    public static long insertionSort(List<Long> arr) {
         // Write your code here
-        int count = 0;
-        for (int i = 0; i < arr.size() - 1; i++) {
+        long count = 0;
+        List<Long> ar = new ArrayList<>();
+        // System.out.println(arr.size());
+        ar.add(arr.get(0));
+        for (int i = 1; i < arr.size(); i++) {
+            ar.add(arr.get(i));
             count = count + sorting(arr, arr.get(i), i);
         }
         return count;
     }
 
-    private static int sorting(List<Integer> arr, int value, int pos) {
-        int res = 0;
-       
-        for (int i = 1; i < arr.size(); i++) {
-            System.out.println("value= " +value);
-            if (arr.get(i) < value && i < pos) {
-                int v = arr.get(i);
+    private static long sorting(List<Long> arr, Long value, int pos) {
+        long res = 0;
+
+        for (int i = 0; i < arr.size(); i++) {
+            // System.out.println("value= " +value);
+            if (arr.get(i) > value && i < pos) {
+                Long v = arr.get(i);
                 arr.set(i, value);
                 value = v;
                 arr.set(pos, value);
                 res++;
-            }else if(i == pos){
-                System.out.println(arr);
-                return res;
             }
         }
         return res;
