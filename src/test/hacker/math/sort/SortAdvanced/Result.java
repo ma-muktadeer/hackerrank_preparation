73089eceb215bb98bbe21db5a7b09a7b3a8a0038
ma-muktadeer <muktadeer.ma@gmail.com>
2025-03-15 12:09:04 +0600

    80%

--- src/test/hacker/math/sort/SortAdvanced/Result.java
+++ src/test/hacker/math/sort/SortAdvanced/Result.java
@@ -4,16 +4,31 @@ import java.util.List;
 
 public class Result {
     public static int insertionSort(List<Integer> arr) {
-    // Write your code here
-    int count = 0;
-        for (int i = 0; i < arr.size(); i++) {
-            count = count + sorting(arr, arr.get(i));
+        // Write your code here
+        int count = 0;
+        for (int i = 0; i < arr.size() - 1; i++) {
+            count = count + sorting(arr, arr.get(i), i);
         }
         return count;
     }
 
-    private int sorting(List<Integer> arr, int value){
-        
+    private static int sorting(List<Integer> arr, int value, int pos) {
+        int res = 0;
+       
+        for (int i = 1; i < arr.size(); i++) {
+            System.out.println("value= " +value);
+            if (arr.get(i) < value && i < pos) {
+                int v = arr.get(i);
+                arr.set(i, value);
+                value = v;
+                arr.set(pos, value);
+                res++;
+            }else if(i == pos){
+                System.out.println(arr);
+                return res;
+            }
+        }
+        return res;
     }
-    
+
 }
--- src/test/hacker/math/sort/SortAdvanced/SortAdvanced.java
+++ src/test/hacker/math/sort/SortAdvanced/SortAdvanced.java
@@ -1,13 +1,15 @@
 package test.hacker.math.sort.SortAdvanced;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class SortAdvanced {
     
     public static void main(String[] args) {
+        List<Integer> ar = Arrays.asList(2, 5, 1, 3, 4, 1);
 
-        int res = Result.insertionSort(Arrays.asList(5, 4, 3, 2, 1));
+        int res = Result.insertionSort(ar);
 
-        System.out.println("Median= " + res);
+        System.out.println("Count= " + res);
     }
 }
