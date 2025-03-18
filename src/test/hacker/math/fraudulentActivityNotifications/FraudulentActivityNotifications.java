a082178f549cdb672f4b4695e5e235fe6908358e
ma-muktadeer <muktadeer.ma@gmail.com>
2025-03-18 14:29:08 +0600

    ok

--- src/test/hacker/math/fraudulentActivityNotifications/FraudulentActivityNotifications.java
+++ src/test/hacker/math/fraudulentActivityNotifications/FraudulentActivityNotifications.java
@@ -4,6 +4,6 @@ import java.util.List;
 
 public class FraudulentActivityNotifications {
     public static void main(String[] args) {
-        System.out.println(Result.activityNotifications(List.of(2, 3, 4, 2, 3, 6, 8, 4, 5), 5));
+        System.out.println(Result.activityNotifications(List.of(10, 20, 30, 40, 50), 3));
     }
 }
--- src/test/hacker/math/fraudulentActivityNotifications/Result.java
+++ src/test/hacker/math/fraudulentActivityNotifications/Result.java
@@ -1,31 +1,53 @@
 package test.hacker.math.fraudulentActivityNotifications;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Result {
 
     public static int activityNotifications(List<Integer> expenditure, int d) {
-        int notification = 0;
-        int lpc = expenditure.size() - d;
+        int notifications = 0;
+        int[] count = new int[expenditure.stream().max(Integer::compareTo).get() + 1];
 
-        for (int i = 0; i < lpc ; i++) {
-            List<Integer> sList = expenditure.subList(i, i + d);
-            notification += findNotification(sList, expenditure.get(i + d));
+        for (int i = 0; i < d; i++) {
+            count[expenditure.get(i)]++;
         }
 
-        return notification;
+        for (int i = d; i < expenditure.size(); i++) {
+            double median = getMedian(count, d);
+
+            if (expenditure.get(i) >= 2 * median) {
+                notifications++;
+            }
+
+            count[expenditure.get(i - d)]--;
+            count[expenditure.get(i)]++;
+        }
+
+        return notifications;
     }
 
-    private static int findNotification(List<Integer> sList, Integer checkValue) {
-        List<Integer> srtList = sList.stream().sorted().collect(Collectors.toList());
-        int mid = srtList.size() / 2;
-        int median = srtList.size() % 2 == 0 ? srtList.get(mid) : (srtList.get(mid) + srtList.get(mid + 1)) / 2;
-        if(checkValue >= 2 * median){
-            return 1;
-        }else{
-            return 0;
+    private static double getMedian(int[] count, int d) {
+        int sum = 0;
+        int mid1 = -1, mid2 = -1;
+        boolean even = d % 2 == 0;
+        int mid = d / 2;
+
+        for (int i = 0; i < count.length; i++) {
+            sum += count[i];
+
+            if (even) {
+                if (mid1 == -1 && sum >= mid)
+                    mid1 = i;
+                if (mid2 == -1 && sum >= mid + 1) {
+                    mid2 = i;
+                    return (mid1 + mid2) / 2.0;
+                }
+            } else {
+                if (sum > mid)
+                    return i;
+            }
         }
+        return 0;
     }
 
 }
