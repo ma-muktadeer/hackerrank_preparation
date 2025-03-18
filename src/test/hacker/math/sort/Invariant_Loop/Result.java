f037681786d65688d0e985a5f8357b798762858f
ma-muktadeer <muktadeer.ma@gmail.com>
2025-03-18 12:12:10 +0600

    ok

--- src/test/hacker/math/sort/Invariant_Loop/Result.java
+++ src/test/hacker/math/sort/Invariant_Loop/Result.java
@@ -1,44 +1,62 @@
 package test.hacker.math.sort.Invariant_Loop;
 
 import java.util.List;
-import java.util.stream.Stream;
 
 public class Result {
+    private static int swapCount = 0;
+    public static void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = left + (right - left) / 2;
 
-    public static void insertionSort1(int n, List<Integer> arr) {
-        
-        int s = 0;
-        Integer[] res = new Integer[n];
-        for (int j = 0; j < n; j++) {
-            res[j] = arr.get(j);
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+            merge(arr, left, mid, right);
         }
-
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 1; j < n; j++) {
-                s = s + isGreater(res, res[j - 1], res[j], j);
-
-            }
-            Stream.of(res).forEach(f -> System.out.print(f + " "));
-            // System.out.println(s);
-        }
-
-        Stream.of(res).forEach(f -> System.out.print(f + " "));
-        System.out.print(s);
     }
 
-    private static Integer isGreater(Integer[] arr, Integer mValue, Integer checkValue, int pos) {
-        int s = 0;
+    public static void merge(int[] arr, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
 
-        if (mValue > checkValue) {
-            s++;
-            int rp = arr[pos - 1];
-            arr[pos - 1] = checkValue;
-            arr[pos] = rp;
-            checkValue = rp;
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        for (int i = 0; i < n1; i++)
+            L[i] = arr[left + i];
+        for (int j = 0; j < n2; j++)
+            R[j] = arr[mid + 1 + j];
+
+        int i = 0, j = 0, k = left;
+
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+                swapCount += (mid + 1) - (left + i); // Count inversions
+            }
+            k++;
         }
-        
-        System.out.println("sort= " + s);
-        return s;
+
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    public static void insertionSort1(int n, List<Integer> arr) {
+        int[] array = arr.stream().mapToInt(i -> i).toArray();
+        mergeSort(array, 0, n - 1);
+        System.out.println(swapCount);
     }
 
 }
