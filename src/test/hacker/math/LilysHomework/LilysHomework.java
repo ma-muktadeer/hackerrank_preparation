2d4f68dc139347705bf3b77b64672592cfb8c65b
ma-muktadeer <muktadeer.ma@gmail.com>
2025-03-18 15:45:40 +0600

    ok

--- src/test/hacker/math/LilysHomework/LilysHomework.java
+++ src/test/hacker/math/LilysHomework/LilysHomework.java
@@ -4,8 +4,8 @@ import java.util.List;
 
 public class LilysHomework {
     public static void main(String[] args) {
-        long result = Result.lilysHomework(List.of(2, 5, 3, 1));
-        System.out.println(result);
+        long result = Result.lilysHomework(List.of(7, 15, 12, 3));
+        System.out.println(result);//2,4,1,5,3//1,4,2,5,3//
     }
     
 }
--- src/test/hacker/math/LilysHomework/Result.java
+++ src/test/hacker/math/LilysHomework/Result.java
@@ -1,6 +1,7 @@
 package test.hacker.math.LilysHomework;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Result {
 
@@ -9,10 +10,12 @@ public class Result {
         long count = 0;
         for (int i = 0; i < ar.length - 1; i++) {
             boolean isSwap = swapValue(ar, i);
-            if(isSwap){
+            if (isSwap) {
                 count += 1;
             }
         }
+
+        // System.out.println(Stream.of(ar).toList());
 
         return count;
     }
@@ -20,15 +23,28 @@ public class Result {
     private static boolean swapValue(Integer[] ar, int i) {
         Integer checkValue = ar[i];
         boolean isSwap = false;
-        for (int j = i + 1; j < ar.length; j++) {
-            if(checkValue > ar[j]){
-                ar[i] = ar[j];
-                ar[j]=checkValue;
+        for (int j = i + 1; j < ar.length; j++) { // 7, 15, 12, 3
+            if (checkValue > ar[j]) {
+                if (i + 1 == j) {
+                    Integer v = checkValue;
+                    ar[i] = ar[j];
+                    ar[j] = v;
+                    checkValue = ar[i];
+                } else {
+                    for (int k = i + 1; k < j; k++) {
+                        Integer v = checkValue;
+                        ar[k] = ar[j];
+                        ar[j] = v;
+                        checkValue = ar[k];
+                    }
+                }
                 isSwap = true;
-            }else{
+            } else {
                 continue;
             }
         }
+        System.out.println(Stream.of(ar).toList());
+
         return isSwap;
     }
 
