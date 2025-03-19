60c3a694978e2c0f7f739c449fa9ba5b32234d60
ma-muktadeer <muktadeer.ma@gmail.com>
2025-03-19 12:28:57 +0600

    ok

--- src/test/hacker/math/LilysHomework/Result.java
+++ src/test/hacker/math/LilysHomework/Result.java
@@ -1,25 +1,56 @@
 package test.hacker.math.LilysHomework;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Result {
     public static long lilysHomework(List<Integer> arr) {
-        
-        long count = 0;
-        for (int i = 0; i < arr.size() - 2; i++) {
-            boolean swp = isSwapValue(arr.subList(0, i + 2), arr.subList(i + 2, arr.size()));
-            if (swp) {
-                count += 1;
+        return Math.min(minSwap(arr, true), minSwap(arr, false));
+    }
+
+    private static long minSwap(List<Integer> arr, boolean ascending){
+        int n = arr.size();
+        int[] original = arr.stream().mapToInt(Integer::intValue).toArray();
+        int[] sorter = Arrays.copyOf(original, n);
+
+        Arrays.sort(sorter);
+
+        if(!ascending){
+            for (int i = 0; i < n /2; i++) {
+                int temp = sorter[i];
+                sorter[i] = sorter[n - i -1];
+                sorter[n-i-1] = temp;
             }
         }
 
-        return count;
-    }
+        Map<Integer, Integer> indexMap = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            indexMap.put(original[i], i);
+        }
 
-    private static boolean isSwapValue(List<Integer> subList, List<Integer> subList2) {
-        int m1 = subList.stream().max(Integer::compareTo).get();
-        int m2 = subList2.stream().min(Integer::compareTo).get();
+        boolean[] visied = new boolean[n];
+        long swaps = 0;
 
-        return m1 > m2;
+        for (int i = 0; i < n; i++) {
+            if(visied[i]  && indexMap.get(sorter[i]) == i){
+                continue;
+            }
+
+            int cycleSize = 0;
+            int j =i;
+
+            while (!visied[j]) {
+                visied[j] = true;
+                j = indexMap.get(sorter[j]);
+                cycleSize++;
+            }
+
+            if(cycleSize>1){
+                swaps+=cycleSize - 1;
+            }
+        }
+        return swaps;
     }
 }
